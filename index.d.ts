2fe542a207b25e30a83473238b97111fb19b8c8e
hongmaoxiao <769501294@qq.com>
2021-09-06 02:51:27 +0800

    Don't require the argument `query` for `question()` (#62)

--- index.d.ts
+++ index.d.ts
@@ -29,7 +29,7 @@ export function cd(path: string)
 
 type QuestionOptions = { choices: string[] }
 
-export function question(query: string, options?: QuestionOptions): Promise<string>
+export function question(query?: string, options?: QuestionOptions): Promise<string>
 
 export class ProcessOutput {
   readonly exitCode: number
